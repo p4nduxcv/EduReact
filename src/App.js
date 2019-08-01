da2acec1db74e29ef1022ceba1c28f1570962d00
Pandula Mandawala <pandula95@gmail.com>
2019-08-01 14:05:00 +0530

    working with style Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from '../src/Person/Person';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`
+
+class App extends Component {
+
+  state = {
+    person: [
+      { name: 'Kukku', age: '25' },
+      { name: 'Arju', age: '23' }
+    ]
+  }
+
+  setButtonAction = (newName) => {
+    this.setState( {
+      person: [
+        { name: 'Kukku Arju', age: '25' },
+        { name: newName, age: '58' },
+        { name: 'Anna Porju', age: '24' }
+      ]
+    })
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState( {
+      person: [
+        { name: 'Kukku Arju', age: '25' },
+        { name: event.target.value, age: '58' },
+        { name: 'Anna Porju', age: '24' }
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi i am react App</h1>
+        <p>This is really working</p>
+        <Button onClick={() => this.setButtonAction('Pandula Cardo')}>Switch Name</Button>
+        <Person 
+        name={this.state.person[0].name} 
+        age={this.state.person[0].age} 
+        click={this.setButtonAction.bind(this,'My3Cardo')} 
+        />
+
+        <Person name={this.state.person[1].name} 
+        age={this.state.person[1].age} 
+        click={this.setButtonAction.bind(this,'Aponsu')} 
+        changed={this.nameChangeHandler}
+        />
+
+        <Person name="arju" age="24" />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+
+const Person = ( props ) => {
+    return (
+        <div>
+            <p onClick={props.click}>I'm {props.name}  and I am {props.age} years old!</p> 
+            <input type="text" onChange={props.changed} value={props.name} /> 
+        </div>
+    )
+
+}
+
+export default Person;
